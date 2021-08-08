d82997dc98683564f327c851d7173593e97e5c14
matiaskaro <85189395+matiaskaro@users.noreply.github.com>
2021-08-08 19:50:56 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head><title>EXAMEN COBRO DIGITAL</title></head>
+<body>
+
+<H1 align ="center"> INTERPRETADOR DE ARCHIVOS </ h1>
+<ol align = "center">
+  <li><a href="Interpretaren.rev.php">INTERPRETA ARCHIVO REV</a></li>
+  <li><a href="Interpretaren.cob.php">INTERPRETA ARCHIVO COB</a></li>
+  <li><a href="Interpreta888ENTES.php">INTERPRETA ARCHIVO 888ENTES</a></li>
+</ol>
+</body>
+</html>
